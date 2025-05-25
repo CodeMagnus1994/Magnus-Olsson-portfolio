9597518859e29e9c1a49961d22053a44256420f9
CodeMagnus1994 <magnusolsson1994@hotmail.se>
2025-05-25 16:33:27 +0200

    Finnished about section, changed favicon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useState } from 'react'
 
 function About() {
+ 
+  const [showTech, setShowTech] = useState("languages")
+
   return (
     <div id="about-layout">
       
@@ -10,7 +14,7 @@ function About() {
         <div>
         <h2>Magnus Olsson</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-           Enim beatae autem distinctio earum porro veritatis dolorum dolore, 
+           Enim beatae autem distinctio earum porro veritatis dolorum dolorfe, 
            ullam modi, sed assumenda
            temporibus fuga similique consequuntur 
            blanditiis ea quae vel natus!
@@ -18,27 +22,53 @@ function About() {
         </div>
       </section>
     
-      <section id="second-about-section" className='about-section'>
-        <div id="programming-languages-images">
-          <button>Programming Langugages</button>
-          <img className='skill-images' src="src/tech-images/java-logo.svg" alt="java-logo"/>
-          <img className='skill-images' src="src/tech-images/css.svg" alt="css"/>
-          <img style={{width: "135px", height: "135px"}} className='skill-images' src="src/tech-images/c++.svg.png" alt="c++"/>
-          <img className='skill-images' src="src/tech-images/html.svg.png" alt="html"/>
-          <img className='skill-images' src="src/tech-images/javascript.svg.png" alt="html"/>
-        </div>
+    <ul id="about-tech-buttons-list">
+      <li><button className='tech-button-class' onClick={() => setShowTech("languages")}>Languages</button></li>
+      <li><button className='tech-button-class' onClick={() => setShowTech("framework")}>Frameworks</button></li>
+      <li><button className='tech-button-class' onClick={() => setShowTech("cloudservices")}>Cloudservicess</button></li>
+      <li><button className='tech-button-class' onClick={() => setShowTech("tools")}>Tools</button></li>
+      <li><button className='tech-button-class' onClick={() => setShowTech("databases")}>Databases</button></li>
+    </ul>
 
-        <div id="framework-images">
-          <button>Frameworks</button>
-          <div style={{border: "2px solid black"}}>
-            <img className='skill-images' src="src/tech-images/spring_Boot.svg.png" alt="Spring Boot"/>
-            <h4 style={{color: "rgb(15, 172, 67)"}}>Spring Boot</h4>
-          </div>
-          <img className='skill-images' src="src/tech-images/logo-react.svg" alt="React JS"/>
+      <section id="second-about-section" className='about-section'>
+        
+        {showTech === "languages" && (
+         <div class="tech-images">
+          <img className='skill-images' src="src/tech-images/java.svg" alt="java-logo"/>
+          <img className='skill-images' src="src/tech-images/javascript.svg" alt="typescript"/>
+          <img className='skill-images' src="src/tech-images/typescript-svgrepo-com.svg" alt="typescript"/>
+          <img className='skill-images' src="src/tech-images/html-5.svg" alt="html"/>
+          <img className='skill-images' src="src/tech-images/css-3.svg" alt="javascript"/>
         </div>
+        )}
+
+        {showTech === "framework" && (
+        <div class="tech-images">
+            <img style={{height: "130px", width: "130px"}} className='skill-images' src="src/tech-images/spring.svg" alt="Spring Boot"/>
+            <img className='skill-images' src="src/tech-images/react-js.svg" alt="React JS"/>
+        </div>  
+        )}
+
+        {showTech === "cloudservices" && (
+        <div class="tech-images">
+            <img style={{backgroundColor: "white", borderRadius: "35%", marginTop: "10px"}} className='skill-images' src="src/tech-images/aws.svg" alt="Amazon Web Services"/>
+        </div>  
+        )}
+
+        {showTech === "databases" && (
+        <div class="tech-images">
+            <img style={{marginTop: "10px"}} className='skill-images' src="src/tech-images/postgresql.svg" alt="PostgreSQL"/>
+        </div>  
+        )}
+
+        {showTech === "tools" && (
+        <div class="tech-images">
+            <img style={{backgroundColor: "white", borderRadius: "25%", marginTop: "10px"}} className='skill-images' src="src/tech-images/github.svg" alt="Github"/>
+        </div>  
+        )}
+        
       </section>
       </div>
-
     </div>
   )
 }
