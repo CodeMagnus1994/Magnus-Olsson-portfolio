c197acc31db41c91152a3404e3855341c4bb5277
CodeMagnus1994 <magnusolsson1994@hotmail.se>
2025-05-27 18:33:52 +0200

    added motionframe to tech-stack

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Input, Typography, TextField } from '@mui/material';
 import { useState } from 'react'
+import { motion } from "framer-motion";
 
 function About() {
  
@@ -105,72 +106,107 @@ function About() {
       <section id="second-about-section" className='about-section'>
         
         {showTech === "languages" && (
+        <motion.div
+         className="tech-images"
+         initial={{ opacity: 0, y: 50 }}
+         animate={{ opacity: 1, y: 25 }}
+         transition={{ duration: 0.5 }}
+         >
          <div class="tech-images">
           <div>
             <img className='skill-images' src="src/tech-images/java.svg" alt="java-logo"/>
-            <h4>Java</h4>
+            <h4 style={{marginLeft: "55px"}}>Java</h4>
           </div>
           <div>
             <img className='skill-images' src="src/tech-images/javascript.svg" alt="typescript"/>
-            <h4>Javascript</h4>
+            <h4 style={{marginLeft: "55px"}}>Javascript</h4>
           </div>
           <div>
             <img className='skill-images' src="src/tech-images/typescript-svgrepo-com.svg" alt="typescript"/>
-            <h4>Typescript</h4>
+            <h4 style={{marginLeft: "55px"}}>Typescript</h4>
           </div>
           <div>
             <img className='skill-images' src="src/tech-images/html-5.svg" alt="html"/>
-            <h4>Html 5</h4>
+            <h4 style={{marginLeft: "55px"}}>HTML 5</h4>
           </div>
           <div>
              <img className='skill-images' src="src/tech-images/css-3.svg" alt="javascript"/>
-             <h4>Css 3</h4>         
+             <h4 style={{marginLeft: "55px"}}>CSS 3</h4>         
           </div>
         </div>
+        </motion.div>
         )}
 
         {showTech === "framework" && (
+               <motion.div
+         className="tech-images"
+         initial={{ opacity: 0, y: 50 }}
+         animate={{ opacity: 1, y: 25 }}
+         transition={{ duration: 0.5 }}
+         >
         <div class="tech-images">
             <div>
-              <img style={{height: "130px", width: "130px"}} className='skill-images' src="src/tech-images/spring.svg" alt="Spring Boot"/>
-              <h4>Spring Boot</h4>
+              <img style={{height: "130px", width: "150px", marginTop: "25px"}} className='skill-images' src="src/tech-images/spring.svg" alt="Spring Boot"/>
+              <h4 style={{marginLeft: "55px"}}>Spring Boot</h4>
             </div>
             <div>
                <img className='skill-images' src="src/tech-images/react-js.svg" alt="React JS"/>
-               <h4>React JS</h4>
+               <h4 style={{marginLeft: "55px"}}>React JS</h4>
             </div>
         </div>  
+        </motion.div>
         )}
 
         {showTech === "cloudservices" && (
+                       <motion.div
+         className="tech-images"
+         initial={{ opacity: 0, y: 50 }}
+         animate={{ opacity: 1, y: 25 }}
+         transition={{ duration: 0.5 }}
+         >
         <div class="tech-images">
           <div>
-            <img style={{backgroundColor: "white", borderRadius: "35%", marginTop: "10px"}} className='skill-images' src="src/tech-images/aws.svg" alt="Amazon Web Services"/>
-            <h4>Amazon Web Services</h4>
+            <img style={{backgroundColor: "white", borderRadius: "35%", marginTop: "10px", marginLeft: "10px"}} className='skill-images' src="src/tech-images/aws.svg" alt="Amazon Web Services"/>
+            <h4 style={{marginLeft: "35px"}}>Amazon Web Services</h4>
           </div>
         </div>  
+        </motion.div>
         )}
 
         {showTech === "databases" && (
+                           <motion.div
+         className="tech-images"
+         initial={{ opacity: 0, y: 50 }}
+         animate={{ opacity: 1, y: 25 }}
+         transition={{ duration: 0.5 }}
+         >
         <div class="tech-images">
           <div>
             <img style={{marginTop: "10px"}} className='skill-images' src="src/tech-images/postgresql.svg" alt="PostgreSQL"/>
-            <h4>PostgreSQL</h4>
+            <h4 style={{marginLeft: "40px"}}>PostgreSQL</h4>
           </div>
         </div>  
+        </motion.div>
         )}
 
         {showTech === "tools" && (
+                             <motion.div
+         className="tech-images"
+         initial={{ opacity: 0, y: 50 }}
+         animate={{ opacity: 1, y: 25 }}
+         transition={{ duration: 0.5 }}
+         >
         <div class="tech-images">
           <div>
-            <img style={{backgroundColor: "white", borderRadius: "25%", marginTop: "10px"}} className='skill-images' src="src/tech-images/github.svg" alt="Github"/>
-            <h4>Github</h4>
+            <img style={{backgroundColor: "white", borderRadius: "25%", marginTop: "10px", marginLeft: "15px"}} className='skill-images' src="src/tech-images/github.svg" alt="Github"/>
+            <h4 style={{marginLeft: "65px"}}>Github</h4>
           </div>
           <div>
             <img style={{backgroundColor: "white", borderRadius: "25%", marginTop: "10px"}} className='skill-images' src="src/tech-images/materialui.svg" alt="MaterialUI"/>
-            <h4>MaterialUI</h4>
+            <h4 style={{marginLeft: "35px"}}>MaterialUI</h4>
           </div>
         </div>  
+        </motion.div>
         )}
         
       </section>
